3fffd62c5ea24e67cdc9169d820ded0b48a86f3a
Hyunseo Lee <leehyunseo7105@gmail.com>
2020-09-01 23:36:41 +0900

    Add dataset for training

---
+++ detect_from_image.py
@@ -0,0 +1,7 @@
+# Face Mask Detection - detect_from_image.py
+# 2020. 09. 01. by Hyunseo Lee
+
+# Usage
+# python detect_from_image.py --image <Image Location>
+
+# Import necessary packages
---
+++ train.py
@@ -0,0 +1,7 @@
+# Face Mask Detection - train.py
+# 2020. 09. 01. by Hyunseo Lee
+
+# Usage
+# python train.py --dataset <Dataset Location>
+
+# Import necessary packages
